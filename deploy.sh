fe96690323d3a1c55b2428cb6517e285d07f9341
ligan <1106502790@qq.com>
2016-12-16 12:24:45 +0800

    编辑部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 cp /usr/webapp/utopia/server.xml /etc/tomcat7/server.xml
-chmod +x /usr/share/tomcat7/bin/startup.sh  
+
+tomcat_startup_sh = /usr/share/tomcat7/bin/startup.sh
+chmod +x tomcat_startup_sh
+sh tomcat_startup_sh
+  
