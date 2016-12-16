b1850eb52ea2ea36d02c67268a9a55d198d26c74
ligan <1106502790@qq.com>
2016-12-16 13:31:16 +0800

    环境部署完成

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
+#复制tomcat配置文件到指定位置
 cp /usr/webapp/utopia/server.xml /etc/tomcat7/server.xml
-
-tomcat_startup_sh="/usr/share/tomcat7/bin/startup.sh"
-chmod +x $tomcat_startup_sh
-sh $tomcat_startup_sh
-  
+#重启tomcat
+sudo /etc/init.d/tomcat7 restart  
