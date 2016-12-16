29d3a5a5cef2d11582054364e640997acb02c40b
ligan <hzligan@corp.netease.com>
2016-12-16 16:27:58 +0800

    多次部署产生失败问题

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+﻿#!/bin/sh
+
+#清空目录
+dir="/usr/webapp/utopiarelease"
+if [ ! -d "$dir"]; then  
+　　mkdir "$dir"  
+else
+    rm -rf "$dir"
+    mkdir "$dir" 
+fi
+
 #复制tomcat配置文件到指定位置
 cp /usr/webapp/utopia/server.xml /etc/tomcat7/server.xml
 #重启tomcat
