b69cd7f81ab3ed747c1eaedf84bc1bcc59d51328
ligan <1106502790@qq.com>
2016-12-16 12:42:41 +0800

    测试脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cp /usr/webapp/utopia/server.xml /etc/tomcat7/server.xml
 
-tomcat_startup_sh = /usr/share/tomcat7/bin/startup.sh
-chmod +x tomcat_startup_sh
-sh tomcat_startup_sh
+tomcat_startup_sh="/usr/share/tomcat7/bin/startup.sh"
+chmod +x $tomcat_startup_sh
+sh $tomcat_startup_sh
   
